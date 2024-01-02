5bd8008a7d4669861fc5d3186b524994f26b58b0
Rahul Gore <rahulgore575@gmail.com>
2024-01-02 22:29:17 +0530

    added hosted route

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const connectDB = require('./config/database');
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const path = require('path')
-const cors = require('cors');
+var cors = require('cors')
 
 
 
@@ -19,11 +19,11 @@ const user = require('./routes/userRoute');
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 
 
 
 
+app.use(cors())
 app.use("/api/v1", user)
 
 app.use(express.static(path.join(__dirname, "frontend\index.html")));
